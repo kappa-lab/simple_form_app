da0a90fa4989a16a037262e97661b6429d40341b
yamagami kenichi <gamiken@kappa-lab.com>
2022-01-20 13:27:17 +0900

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,15 +15,9 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // expect(find.text('SIMPLE'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.widgetWithText(ElevatedButton, "Sign In"), findsOneWidget);
 
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.widgetWithText(ElevatedButton, "Sign In"));
+    await tester.pump();
   });
 }
